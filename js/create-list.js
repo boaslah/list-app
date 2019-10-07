ce85ff360163df0bf56fe1b0180abaeeba768bdb
boaslah <blessing@kwagei.com>
2019-10-07 12:32:53 +0000

    create-list html  and js files added

---
+++ js/create-list.js
@@ -0,0 +1,11 @@
+var create_list = document.getElementById('create_list');
+var array_of_list = [];
+
+document.getElementById('submit').addEventListener('click', function(){
+    getListName();
+})
+
+function getListName(){
+    array_of_list.push(create_list.value)
+    localStorage.setItem('List-Name', array_of_list);
+}
